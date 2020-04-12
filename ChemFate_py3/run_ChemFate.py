e02f635ebd025f31c067379360c2109fb550bd77
MengyaTao <mengya120@gmail.com>
2020-04-12 12:27:44 -0700

    edit input file names.

--- ChemFate_py3/run_ChemFate.py
+++ ChemFate_py3/run_ChemFate.py
@@ -5,8 +5,6 @@ from model_setup import Model_SetUp
 chem_type = 'NonionizableOrganic' # can only be 'NonionizableOrganic', 'IonizableOrganic', 'Metal', or 'Nanomaterial'
 CUR_PATH = os.path.dirname(os.path.abspath(__file__))
 
-# chem_file = CUR_PATH + '/../Input/ChemParam_ethylbenzene.xlsx'
-
 if chem_type == 'NonionizableOrganic':
     chem_file = CUR_PATH + './Input/ChemParam_nonionizableOrganic.xlsx'
 elif chem_type == 'IonizableOrganic':
@@ -17,11 +15,11 @@ else:
     chem_file = CUR_PATH + './Input/ChemParam_nanomaterial.xlsx'
 
 
-region_file = CUR_PATH + './Input/Visalia.xlsx'
-release_file = CUR_PATH + './Input/ChemRelease_nonionizableOrganic.xlsx'
+region_file = CUR_PATH + './Input/Region.xlsx'
+release_file = CUR_PATH + './Input/ChemRelease.xlsx'
 run_option = 1 # can be 1 or 2:
 bgPercOption2 = 10 # can be anywhere between 0-100
-file_name = 'chlorothalonil' # pyrimethanil, cyprodinil, copper, nanoCopper
+file_name = 'test_run' # pyrimethanil, cyprodinil, copper, nanoCopper
 output_file_path = CUR_PATH + './Output/' + file_name + '/'
 
 if not os.path.exists(output_file_path):
